8dfcfb1d2d3d614547bee77d30f0573030627a59
Jennifer Owens <jmo.jennifer@gmail.com>
2016-12-19 10:56:20 -0800

    Refactored for spot info to better match Wave 1 game logic

--- src/app/views/board_view.js
+++ src/app/views/board_view.js
@@ -5,20 +5,14 @@ import _ from 'underscore';
 var BoardView = Backbone.View.extend({
 
   events: {
-    'click #0': 'markSpot',
-    'click #1': 'markSpot',
-    'click #2': 'markSpot',
-    'click #3': 'markSpot',
-    'click #4': 'markSpot',
-    'click #5': 'markSpot',
-    'click #6': 'markSpot',
-    'click #7': 'markSpot',
-    'click #8': 'markSpot',
+    'click .square': 'markSpot'
   },
 
-  markSpot: function(spot_id) {
-    console.log("Pressed a spot!");
-    }
+  markSpot: function(spot) {
+    var spotVertical = spot.currentTarget.attributes.vertical.value;
+    var spotHorizontal = spot.currentTarget.attributes.horizontal.value;
+    console.log('Spot [' + spotVertical + ',' + spotHorizontal + ']');
+  }
 
   }
 );
